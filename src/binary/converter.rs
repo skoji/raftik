d84bc1306715c9526a1ea93704444be9f6210627
Satoshi Kojima <skoji@skoji.jp>
2025-08-11 10:39:30 +0900

    implement parsing sections first step

--- src/binary/converter.rs
+++ src/binary/converter.rs
@@ -24,6 +24,20 @@ mod tests {
     }
 
     #[test]
+    fn test_module_with_only_type_section() {
+        let wasm = wat::parse_str(
+            "(module
+                (type (func))
+            )",
+        )
+        .unwrap();
+        let module: Module = Module::try_from(wasm.as_ref()).unwrap();
+        assert_eq!(module.magic, [0x00, 0x61, 0x73, 0x6d]);
+        assert_eq!(module.version, 1);
+        assert_eq!(module.sections.len(), 1);
+    }
+
+    #[test]
     fn test_module_try_from_short_data() {
         let data: &[u8] = &[0x4d, 0x4f];
         let result = Module::try_from(data);
