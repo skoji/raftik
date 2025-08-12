15b8faef5db98a49feecea96832d18ea975a7c5e
Satoshi Kojima <skoji@skoji.jp>
2025-08-12 15:19:37 +0900

    introduce Module again

--- src/ast.rs
+++ src/ast.rs
@@ -1,2 +1,9 @@
 pub mod section;
 pub mod types;
+
+use section::TypeSection;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Module {
+    pub type_section: Option<TypeSection>,
+}
--- src/binary/converter.rs
+++ src/binary/converter.rs
@@ -1,1 +1,17 @@
+use super::parser::parse_raw_module;
+use crate::ast::Module;
 
+impl TryFrom<&[u8]> for Module {
+    type Error = String;
+
+    fn try_from(input: &[u8]) -> Result<Self, Self::Error> {
+        let (remaining, _) =
+            parse_raw_module(input).map_err(|e| format!("Failed to parse raw module: {}", e))?;
+
+        if !remaining.is_empty() {
+            return Err("Extra data after module".to_string());
+        }
+
+        Ok(Module { type_section: None })
+    }
+}
