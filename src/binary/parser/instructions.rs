4776be736dcea7f8f5cf3a3911177e1ccf3b277c
Satoshi Kojima <skoji@skoji.jp>
2025-08-17 13:38:12 +0900

    introduce const for 0x0b, which means end opcode

--- src/binary/parser/instructions.rs
+++ src/binary/parser/instructions.rs
@@ -7,9 +7,11 @@ use nom::{
 
 use crate::ast::instructions::Expression;
 
+const END_OPCODE: u8 = 0x0b;
+
 pub fn parse_expression(input: &[u8]) -> IResult<&[u8], Expression<'_>> {
     map(
-        terminated(take_until(&[0x0b][..]), tag(&[0x0b][..])),
+        terminated(take_until(&[END_OPCODE][..]), tag(&[END_OPCODE][..])),
         |instructions| Expression { instructions },
     )
     .parse(input)
