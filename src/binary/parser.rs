beb2322631c154d3daa6cee18d6da77653926c57
Satoshi Kojima <skoji@skoji.jp>
2025-08-20 07:51:01 +0900

    introduce test utility : test_with_wat

--- src/binary/parser.rs
+++ src/binary/parser.rs
@@ -47,173 +47,186 @@ mod tests {
         section::{Export, ExportDesc, Global, Import, ImportDesc, SectionID},
         types::*,
     };
+
+    fn with_wat(wat: impl AsRef<str>, test: impl Fn(Module)) {
+        let wasm = wat::parse_str(wat).unwrap();
+        let module = Module::from_bytes(&wasm).unwrap();
+        test(module)
+    }
+
     #[test]
     fn test_minimal_wasm() {
-        let wasm = wat::parse_str("(module)").unwrap();
-        let module = Module::from_bytes(wasm.as_ref()).unwrap();
-        assert_eq!(module, Module::default());
+        with_wat("(module)", |module| assert_eq!(module, Module::default()));
     }
 
     #[test]
     fn test_wasm_with_type_section() {
-        let wasm = wat::parse_str("(module (type (func (param i32 i32) (result i64))))").unwrap();
-        let module = Module::from_bytes(wasm.as_ref()).unwrap();
-        assert_eq!(
-            module,
-            Module {
-                sections: vec![Section::Type(TypeSection {
-                    types: vec![FunctionType {
-                        params: vec![
-                            ValueType::Number(NumberType::I32),
-                            ValueType::Number(NumberType::I32)
-                        ],
-                        results: vec![ValueType::Number(NumberType::I64)]
-                    }]
-                })]
-            }
+        with_wat(
+            "(module (type (func (param i32 i32) (result i64))))",
+            |module| {
+                assert_eq!(
+                    module,
+                    Module {
+                        sections: vec![Section::Type(TypeSection {
+                            types: vec![FunctionType {
+                                params: vec![
+                                    ValueType::Number(NumberType::I32),
+                                    ValueType::Number(NumberType::I32)
+                                ],
+                                results: vec![ValueType::Number(NumberType::I64)]
+                            }]
+                        })]
+                    }
+                )
+            },
         );
     }
 
     #[test]
     fn test_wasm_with_import_section() {
-        let wasm = wat::parse_str("(module (import \"console\" \"log\" (func $log (param i32))))")
-            .unwrap();
-        let module = Module::from_bytes(wasm.as_ref()).unwrap();
-        // type section and import section exists.
-        assert_eq!(module.sections.len(), 3);
-        assert_eq!(
-            module.sections[0],
-            Section::Type(TypeSection {
-                types: vec![FunctionType {
-                    params: vec![ValueType::Number(NumberType::I32)],
-                    results: vec![]
-                }]
-            })
-        );
-        assert_eq!(
-            module.sections[1],
-            Section::Import(ImportSection {
-                imports: vec![Import {
-                    module: "console".to_string(),
-                    name: "log".to_string(),
-                    desc: ImportDesc::TypeIndex(0)
-                }]
-            })
+        with_wat(
+            "(module (import \"console\" \"log\" (func $log (param i32))))",
+            |module| {
+                // type section and import section exists.
+                assert_eq!(module.sections.len(), 3);
+                assert_eq!(
+                    module.sections[0],
+                    Section::Type(TypeSection {
+                        types: vec![FunctionType {
+                            params: vec![ValueType::Number(NumberType::I32)],
+                            results: vec![]
+                        }]
+                    })
+                );
+                assert_eq!(
+                    module.sections[1],
+                    Section::Import(ImportSection {
+                        imports: vec![Import {
+                            module: "console".to_string(),
+                            name: "log".to_string(),
+                            desc: ImportDesc::TypeIndex(0)
+                        }]
+                    })
+                )
+            },
         );
     }
     #[test]
     fn test_wasm_with_function_section() {
-        let wasm =
-            wat::parse_str("(module (func (param $l i32) (result i32) local.get $l))").unwrap();
-        let module = Module::from_bytes(wasm.as_ref()).unwrap();
-        assert_eq!(module.sections.len(), 4);
-        assert_eq!(
-            module.sections[0],
-            Section::Type(TypeSection {
-                types: vec![FunctionType {
-                    params: vec![ValueType::Number(NumberType::I32)],
-                    results: vec![ValueType::Number(NumberType::I32)]
-                }]
-            })
-        );
-        assert_eq!(
-            module.sections[1],
-            Section::Function(FunctionSection {
-                type_indexes: vec![0]
-            })
+        with_wat(
+            "(module (func (param $l i32) (result i32) local.get $l))",
+            |module| {
+                assert_eq!(module.sections.len(), 4);
+                assert_eq!(
+                    module.sections[0],
+                    Section::Type(TypeSection {
+                        types: vec![FunctionType {
+                            params: vec![ValueType::Number(NumberType::I32)],
+                            results: vec![ValueType::Number(NumberType::I32)]
+                        }]
+                    })
+                );
+                assert_eq!(
+                    module.sections[1],
+                    Section::Function(FunctionSection {
+                        type_indexes: vec![0]
+                    })
+                );
+            },
         );
     }
 
     #[test]
     fn test_wasm_with_table_section() {
-        let wasm = wat::parse_str("(module (table 1 10 funcref))").unwrap();
-        let module = Module::from_bytes(wasm.as_ref()).unwrap();
-        assert_eq!(module.sections.len(), 1);
-        assert_eq!(
-            module.sections[0],
-            Section::Table(TableSection {
-                tables: vec![TableType {
-                    ref_type: ReferenceType::FuncRef,
-                    limits: Limits {
-                        min: 1,
-                        max: Some(10)
-                    }
-                }]
-            })
-        );
+        with_wat("(module (table 1 10 funcref))", |module| {
+            assert_eq!(module.sections.len(), 1);
+            assert_eq!(
+                module.sections[0],
+                Section::Table(TableSection {
+                    tables: vec![TableType {
+                        ref_type: ReferenceType::FuncRef,
+                        limits: Limits {
+                            min: 1,
+                            max: Some(10)
+                        }
+                    }]
+                })
+            );
+        });
     }
 
     #[test]
     fn test_wasm_with_memory_section() {
-        let wasm = wat::parse_str("(module (memory 100))").unwrap();
-        let module = Module::from_bytes(wasm.as_ref()).unwrap();
-        assert_eq!(module.sections.len(), 1);
-        assert_eq!(
-            module.sections[0],
-            Section::Memory(MemorySection {
-                memories: vec![MemoryType {
-                    limits: Limits {
-                        min: 100,
-                        max: None,
-                    }
-                }]
-            })
-        )
+        with_wat("(module (memory 100))", |module| {
+            assert_eq!(module.sections.len(), 1);
+            assert_eq!(
+                module.sections[0],
+                Section::Memory(MemorySection {
+                    memories: vec![MemoryType {
+                        limits: Limits {
+                            min: 100,
+                            max: None,
+                        }
+                    }]
+                })
+            );
+        });
     }
 
     #[test]
     fn test_wasm_with_global_section() {
-        let wasm = wat::parse_str("(module (global i32 (i32.const 32)))").unwrap();
-        let module = Module::from_bytes(wasm.as_ref()).unwrap();
-        assert_eq!(module.sections.len(), 1);
-        assert_eq!(
-            module.sections[0],
-            Section::Global(GlobalSection {
-                globals: vec![Global {
-                    global_type: GlobalType {
-                        val_type: ValueType::Number(NumberType::I32),
-                        mutability: Mutability::Const,
-                    },
-                    expression: Expression {
-                        instructions: &[0x41, 0x20][..]
-                    }
-                }]
-            })
-        )
+        with_wat("(module (global i32 (i32.const 32)))", |module| {
+            assert_eq!(module.sections.len(), 1);
+            assert_eq!(
+                module.sections[0],
+                Section::Global(GlobalSection {
+                    globals: vec![Global {
+                        global_type: GlobalType {
+                            val_type: ValueType::Number(NumberType::I32),
+                            mutability: Mutability::Const,
+                        },
+                        expression: Expression {
+                            instructions: &[0x41, 0x20][..]
+                        }
+                    }]
+                })
+            );
+        });
     }
 
     #[test]
     fn test_wasm_with_export_section() {
-        let wasm =
-            wat::parse_str("(module (func (export \"the_answer\") (result i32) i32.const 42))")
-                .unwrap();
-        let module = Module::from_bytes(wasm.as_ref()).unwrap();
-        assert_eq!(module.sections.len(), 4); // Type, Function, Export, Code
-        assert_eq!(module.sections[0].id(), SectionID::Type);
-        assert_eq!(module.sections[1].id(), SectionID::Function);
-        assert_eq!(
-            module.sections[2],
-            Section::Export(ExportSection {
-                exports: vec![Export {
-                    name: "the_answer".to_string(),
-                    desc: ExportDesc::FunctionIndex(0),
-                }]
-            })
-        )
+        with_wat(
+            "(module (func (export \"the_answer\") (result i32) i32.const 42))",
+            |module| {
+                assert_eq!(module.sections.len(), 4); // Type, Function, Export, Code
+                assert_eq!(module.sections[0].id(), SectionID::Type);
+                assert_eq!(module.sections[1].id(), SectionID::Function);
+                assert_eq!(
+                    module.sections[2],
+                    Section::Export(ExportSection {
+                        exports: vec![Export {
+                            name: "the_answer".to_string(),
+                            desc: ExportDesc::FunctionIndex(0),
+                        }]
+                    })
+                );
+            },
+        );
     }
 
     #[test]
     fn test_wasm_with_start_section() {
-        let wasm = wat::parse_str("(module (func) (start 0))").unwrap();
-        let module = Module::from_bytes(wasm.as_ref()).unwrap();
-        assert_eq!(module.sections.len(), 4); // Type, Function, Start, Code
-        assert_eq!(module.sections[0].id(), SectionID::Type);
-        assert_eq!(module.sections[1].id(), SectionID::Function);
-        assert_eq!(
-            module.sections[2],
-            Section::Start(StartSection {
-                start_function_index: 0
-            })
-        );
+        with_wat("(module (func) (start 0))", |module| {
+            assert_eq!(module.sections.len(), 4); // Type, Function, Start, Code
+            assert_eq!(module.sections[0].id(), SectionID::Type);
+            assert_eq!(module.sections[1].id(), SectionID::Function);
+            assert_eq!(
+                module.sections[2],
+                Section::Start(StartSection {
+                    start_function_index: 0
+                })
+            );
+        });
     }
 }
