88f37d27249df7fee8bca75447cf4af0246ae435
Satoshi Kojima <skoji@skoji.jp>
2025-08-25 18:13:25 +0900

    write down all match cases of section in validator; almost of it is empty yet

--- raftik-core/src/validation.rs
+++ raftik-core/src/validation.rs
@@ -17,12 +17,22 @@ pub fn validate_module(module: &ModuleParsed) -> Result<(), ValidationError> {
     let mut context = Context::default();
     for section in module.sections.iter() {
         match section {
-            Section::Type(type_section) => context.type_section = Some(type_section),
+            Section::Type(type_section) => context.type_section = Some(type_section), // no validation needed.
+            Section::Import(_) => (), // TODO; should validate
             Section::Function(function_section) => {
                 context.function_section = Some(function_section);
                 section::validate_function_section(function_section, &context)?
             }
-            _ => (),
+            Section::Table(_) => (),     // TODO; should validate
+            Section::Memory(_) => (),    // TODO; should validate
+            Section::Global(_) => (),    // TODO; should validate
+            Section::Export(_) => (),    // TODO; should validate
+            Section::Start(_) => (),     // TODO; should validate
+            Section::Element(_) => (),   // TODO; should validate
+            Section::Code(_) => (),      // TODO; should validate
+            Section::Data(_) => (),      // TODO; should validate
+            Section::DataCount(_) => (), // TODO; should validate
+            Section::Custom(_) => (),    // no need to validate
         }
     }
     Ok(())
