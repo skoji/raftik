b4256b1e92bdeb123e53a92b65903517107d06b1
Satoshi Kojima <skoji@skoji.jp>
2025-08-26 17:55:24 +0900

    restructure validation context

--- raftik-core/src/validation/error.rs
+++ raftik-core/src/validation/error.rs
@@ -2,6 +2,15 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ValidationError {
+    #[error("{refering} index {refering_index} out of bounds at {refered_index} for {refered}")]
+    IndexOutOfBoundsIn {
+        refering: &'static str,
+        refering_index: usize,
+        refered: &'static str,
+        refered_index: u32,
+    },
+
+    // should never happen
     #[error("{0} Section: No {1} Section found")]
     ReferingSectionNotFound(&'static str, &'static str),
     #[error("{0} Section: {1} index {2} out of bounds at {3} for {1} Section")]
