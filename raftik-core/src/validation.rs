82fe1776db5523fd0038b3a203d191a2fe844e71
Satoshi Kojima <skoji@skoji.jp>
2025-08-26 18:12:25 +0900

    introduce initialize_context and do context initialization before validation

--- raftik-core/src/validation.rs
+++ raftik-core/src/validation.rs
@@ -19,15 +19,14 @@ struct Context<'a> {
     pub locals: Vec<&'a ValueType>,
 }
 
-pub fn validate_module(module: &ModuleParsed) -> Result<(), ValidationError> {
+fn initialize_context<'a>(module: &'a ModuleParsed<'a>) -> Result<Context<'a>, ValidationError> {
     let mut context = Context::default();
     for section in module.sections.iter() {
         match section {
             Section::Type(type_section) => context.types = type_section.types.iter().collect(),
-            Section::Import(_) => (), // TODO; should validate
+            Section::Import(_) => (),
             Section::Function(function_section) => {
                 context.functions = function_section.type_indices.iter().collect();
-                section::validate_function_section(function_section, &context)?
             }
             Section::Table(table_section) => context.tables = table_section.tables.iter().collect(),
             Section::Memory(memory_section) => {
@@ -40,6 +39,31 @@ pub fn validate_module(module: &ModuleParsed) -> Result<(), ValidationError> {
                     .map(|g| &g.global_type)
                     .collect()
             }
+            Section::Export(_) => (),
+            Section::Start(_) => (),
+            Section::Element(_) => (),
+            Section::Code(_) => (),
+            Section::Data(_) => (),
+            Section::DataCount(_) => (),
+            Section::Custom(_) => (),
+        }
+    }
+    Ok(context)
+}
+
+pub fn validate_module(module: &ModuleParsed) -> Result<(), ValidationError> {
+    #[allow(unused_mut)]
+    let mut context = initialize_context(module)?;
+    for section in module.sections.iter() {
+        match section {
+            Section::Type(_) => (),   // no need to validate
+            Section::Import(_) => (), // TODO; should validate
+            Section::Function(function_section) => {
+                section::validate_function_section(function_section, &context)?
+            }
+            Section::Table(_) => (),  // TODO; should validate
+            Section::Memory(_) => (), // TODO; should validate
+            Section::Global(_) => (), // TODO; should validate
             Section::Export(export_section) => {
                 section::validate_export_section(export_section, &context)?
             }
