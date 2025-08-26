704ccac52fa771f9f1ea2dafa06a46849beffd03
Satoshi Kojima <skoji@skoji.jp>
2025-08-26 18:16:50 +0900

    cargo fmt

--- raftik-core/src/validation/error.rs
+++ raftik-core/src/validation/error.rs
@@ -2,7 +2,9 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ValidationError {
-    #[error("index {referred_index} out of bounds in {referred} section (referenced from {referring} section at index {referring_index})")]
+    #[error(
+        "index {referred_index} out of bounds in {referred} section (referenced from {referring} section at index {referring_index})"
+    )]
     IndexOutOfBoundsIn {
         referring: &'static str,
         referring_index: usize,
