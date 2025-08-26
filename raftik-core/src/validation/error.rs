4756f1ab62877360465c9c881592b98ae82a4f11
Satoshi Kojima <skoji@skoji.jp>
2025-08-26 18:02:05 +0900

    remove unused errors

--- raftik-core/src/validation/error.rs
+++ raftik-core/src/validation/error.rs
@@ -9,10 +9,4 @@ pub enum ValidationError {
         referred: &'static str,
         referred_index: u32,
     },
-
-    // should never happen
-    #[error("{0} Section: No {1} Section found")]
-    ReferingSectionNotFound(&'static str, &'static str),
-    #[error("{0} Section: {1} index {2} out of bounds at {3} for {1} Section")]
-    IndexOutOfBoundsInSection(&'static str, &'static str, u32, usize),
 }
