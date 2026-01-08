4ac5c97bde4800bdbb9008d38bf245548c7733d2
Satoshi Kojima <skoji@skoji.jp>
2026-01-08 19:31:59 +0900

    parse and validate module before execution

--- raftik-core/src/execution/error.rs
+++ raftik-core/src/execution/error.rs
@@ -1,6 +1,11 @@
+use crate::validation::error::ValidationError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    
+    #[error("parse error:{parse_error}")]
+    ParseError { parse_error: String },
+
+    #[error("validation error:{validation_error}")]
+    ValidationError { validation_error: ValidationError },
 }
--- raftik-core/src/execution/instance.rs
+++ raftik-core/src/execution/instance.rs
@@ -1,13 +1,20 @@
+use super::error::Error;
 use super::{rawinstance::Module as RawModule, store::Store};
+use crate::{ast::ModuleParsed, validation::validate_module};
 
 struct Module {
     index: usize,
 }
 
 impl Module {
-    fn new(_wat: &[u8], store: &mut Store) -> Self {
+    fn from_slice(wat: &[u8], store: &mut Store) -> Result<Self, Error> {
+        let module =
+            ModuleParsed::from_slice(wat).map_err(|e| Error::ParseError { parse_error: e })?;
+        validate_module(&module).map_err(|e| Error::ValidationError {
+            validation_error: e,
+        })?;
         let raw_module = RawModule::default();
         let index = store.register_module(raw_module);
-        Module { index }
+        Ok(Module { index })
     }
 }
